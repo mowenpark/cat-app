5b278745b50c8de4160a546388e49af632978c85
Michael Park <michaelowenpark@gmail.com>
2016-01-26 14:56:22 -0800

    add edit, partial form, and edit CatsController

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -10,16 +10,33 @@ class CatsController < ApplicationController
   end
 
   def new
+    @cat = Cat.new(name: nil, color: nil, birth_date: nil, description: nil, sex: nil)
     render :new
   end
 
   def create
     @cat = Cat.new(cat_params)
     if @cat.save
-      redirect_to :index
+      redirect_to cat_url(@cat)
     else
       raise "HELL"
     end
+  end
+
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find_by(id: params[:id])
+    @cat.update_attributes!(cat_params)
+    redirect_to cat_url(@cat)
+    # if @cat.update
+    #   redirect_to :show
+    # else
+    #   raise "HEEEELLLLLLLL"
+    # end
   end
 
   private
